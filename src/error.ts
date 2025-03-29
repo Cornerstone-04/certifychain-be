8a5ac55f7769fd7146f220fcdcc7c2f7baaeb509
Barry-san <oyeyemimubarak6@gmail.com>
2025-03-29 17:51:53 +0100

    feat: craeted custom appError class

---
+++ src/error.ts
@@ -0,0 +1,10 @@
+export class AppError extends Error {
+  public statuscode: number;
+  public isoperational: boolean;
+
+  constructor(statuscode: number, message?: string, isoperational = true) {
+    super(message);
+    this.isoperational = isoperational;
+    this.statuscode = statuscode;
+  }
+}
