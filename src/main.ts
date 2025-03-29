6a80092075ddbc6ddf5200ff104ebd043cca69be
Barry-san <oyeyemimubarak6@gmail.com>
2025-03-29 17:52:32 +0100

    feat: added error handling, catch all route, better organization

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,66 @@
-import express from "express";
+import express, { ErrorRequestHandler, Express, Handler, json } from "express";
 
-export const app = express();
-app.get("/", (_, res) => {
-  res.json({ message: "hello world" });
-});
+import type { Helia } from "helia";
+import { createHelia } from "helia";
+import verify from "./features/verify/verifyHandlers.js";
+import { uploadRouter } from "./features/upload/uploadHandler.js";
 
-app.listen(3000);
+import { StatusCodes } from "http-status-codes";
+import { AppError } from "./errror.js";
+
+type Server = {
+  app: Express;
+  helia: Helia | null;
+  startServer: () => void;
+  initHelia: () => void;
+};
+
+export const server: Server = {
+  app: express(),
+  helia: null,
+
+  startServer() {
+    if (!this.helia) {
+      this.initHelia();
+    }
+    this.app.listen(3000);
+  },
+  async initHelia() {
+    const h = await createHelia();
+    this.helia = h;
+    this.helia.start();
+  },
+};
+
+const rootHandler: Handler = (req, res, next) => {
+  res.status(200).json({
+    isSuccess: true,
+    message: "this is the root route",
+  });
+};
+
+const errorHanlder: ErrorRequestHandler = (err, req, res, next) => {
+  if (err instanceof AppError) {
+    res.status(err.statuscode).json({
+      isSuccess: false,
+      message: err.message,
+    });
+  }
+};
+
+const CatchAll: Handler = (req, res) => {
+  res.status(StatusCodes.NOT_FOUND).json({
+    isSuccess: false,
+    message: `The resource you're looking for at ${req.method} ${req.baseUrl}${req.path} does not exist`,
+  });
+};
+
+const { app } = server;
+
+app.use(json());
+app.get("/", rootHandler);
+app.use(uploadRouter);
+app.use(errorHanlder);
+app.use("/*", CatchAll);
+
+server.startServer();
