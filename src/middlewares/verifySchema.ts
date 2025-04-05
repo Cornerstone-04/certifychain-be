f28d8cab12a793f64eaf1e72dd98d23d735deef9
Barry-san <oyeyemimubarak6@gmail.com>
2025-04-05 13:56:54 +0100

    fix : added next functio to verification middleware

--- src/middlewares/verifySchema.ts
+++ src/middlewares/verifySchema.ts
@@ -2,10 +2,13 @@ import { ifError } from "assert";
 import { Handler } from "express";
 import { ZodError, ZodSchema } from "zod";
 
-const verifySchema = (ShemaDef: ZodSchema): Handler => {
+export const verifySchema = (ShemaDef: ZodSchema): Handler => {
+  console.log("verifying request body");
   return (req, res, next) => {
     try {
       ShemaDef.parse(req.body);
+      console.log("request body verified");
+      next();
     } catch (err) {
       if (err instanceof ZodError) {
         res.json({
