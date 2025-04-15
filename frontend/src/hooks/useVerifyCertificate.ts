56eec47e905a398837af785e80078e44f7d23479
Cornerstone-04 <fortunecornerstone@gmail.com>
2025-04-15 12:40:22 +0100

    chore: separate frontend repo

--- frontend/src/hooks/useVerifyCertificate.ts
+++
@@ -1,8 +0,0 @@
-import { api } from "@/lib/api";
-import { useMutation } from "@tanstack/react-query";
-
-export const useVerifyCertificate = () =>
-  useMutation({
-    mutationFn: (hash: string) =>
-      api.post("/verify/getFile", { hash }).then((res) => res.data),
-  });
