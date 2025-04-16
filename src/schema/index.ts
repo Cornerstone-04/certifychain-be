a70e57dcecc7d97b2685d7d9fc7f0058c44c55d3
Barry-san <oyeyemimubarak6@gmail.com>
2025-04-16 15:07:19 +0100

    chore: updated schema declarations

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 // define schema for validating upload request body
 export const uploadSchema = z.object({
-  // validate 'name' to be a string between 0 and 1024 characters
+  // validate 'name' to be a string with a minimum length of 0 and base64 encoded
   name: z.string().min(0).base64(),
   // validate 'content' to be a string with a minimum length of 0
   content: z.string().min(0),
