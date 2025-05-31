7f430bb7db0cb36d62f52316ba3aaba910cdb375
Barry-san <oyeyemimubarak6@gmail.com>
2025-05-31 19:06:50 +0100

    formdata uploads

--- src/middlewares/verifySchema.ts
+++ src/middlewares/verifySchema.ts
@@ -5,12 +5,10 @@ import { ZodError, ZodSchema } from "zod";
 
 // middleware to validate request body using zod scehma
 export const verifySchema = (ShemaDef: ZodSchema): Handler => {
-  console.log("verifying request body");
   return (req, res, next) => {
     try {
       // parse and validate request body
       ShemaDef.parse(req.body);
-      console.log("request body verified");
       next();
     } catch (err) {
       // handle zod errors
